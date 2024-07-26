59d6f7c92debeefb722326d273e0156fcb451ec7
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-07-25 20:20:11 -0500

    first commit

---
+++ Ejemplo.py
@@ -0,0 +1,13 @@
+x = 34 - 23
+y = "hola"
+z = 3.45
+
+if z == 3.45 and y == "hola":
+    x += 1
+    y = y + "mundo"
+    y += "mundo"
+    
+    y += "muchachos"
+    print(x)
+    print(y)
+    print(z)
